49c882783fb772ef94ba31358d4b6c0eaa4cf70a
kyhyun <k29176@gmail.com>
2022-03-14 00:11:58 +0900

    change : 백준 문제풀이 수정
    boj5598 카이사르 암호

--- BOJ/learning_C++/boj5598.py
+++ BOJ/learning_C++/boj5598.py
@@ -1,10 +1,8 @@
 # boj5598 카이사르 암호
 s = list(input())
-answer = []
 for i in range(len(s)):
-    code = ord(s[i]) + 3
-    if code >= 90:
-        answer[i] = chr(code - 26)
-    else:
-        answer[i] = chr(code)
-print(answer)
+    code = ord(s[i]) - 3
+    if code < 65:
+        code = code + 26
+    s[i] = chr(code)
+print(''.join(s))
