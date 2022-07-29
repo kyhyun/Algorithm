6683cea50596c6604e392059d2594b3ac97158e1
kyhyun <k29176@gmail.com>
2022-07-29 18:43:43 +0900

    refactor : 코드 주석 추가 및 풀이 방법 수정

--- Programmers/Level_1/basic/하샤드의수.js
+++ Programmers/Level_1/basic/하샤드의수.js
@@ -1,10 +1,15 @@
+// 하샤드의 수
+/*
+문제 해결 과정
+[v] 하샤드의 수 이해하기 - x가 각자 자릿수를 더해 합쳐진 수로 나누어져야 한다. 예) 18 => 1 + 8 = 9 >> 18은 9로 나누어진다.
+[v] 나누어 떨어진다는 조건은 x 가 합쳐진 수와 나눌 때 0의 값이 되어야 한다는 조건을 의미
+[v] 숫자를 문자로 바꾼 후 각 자릿수를 모두 더한다. ( 더하기 위해서 sum 변수 시용 )
+[v] x와 그 합친 수를 나누어서 0이 된다면 true를 반환하고 그렇지 않으면 false를 반환한다.
+*/
+
 function solution(x) {
-  let answer = true;
   const str = x.toString();
   let sum = 0;
-
-  for (let i = 0; i < str.length; i += 1) {
-    sum += Number(str[i]);
-  }
-  return x % sum === 0 ? answer : !answer;
+  for (const e of str) sum += Number(e);
+  return x % sum === 0 ? true : false;
 }
--- Programmers/Level_1/basic/핸드폰번호가리기.js
+++ Programmers/Level_1/basic/핸드폰번호가리기.js
@@ -1,5 +1,21 @@
-function solution(s) {
-  let answer = s.slice(s.length - 4);
-  let hiden = "*".repeat(s.length - 4);
-  return hiden + answer;
+// 핸드폰 번호 가리기
+/*
+문제 해결 과정
+[v] 문제 이해하기 - 전화번호 뒤에 4자리를 제외하고 나머지를 ' * ' 로 가린 문자열을 구한다.
+[v] 문자열의 전체 길이를 구한다.
+[v] 반복문을 순회하면서 i가 전체 길이의 - 4(시작 지점 ~ 끝 지점 되기까지 4개 만큼의 길이)일 때 ' * ' 을 넣는다.
+[v] 끝 지점의 4자리만 본래 문자열의 자릿 수를 넣고 결과를 반환한다.
+*/
+
+function solution(phone_number) {
+  let str = '';
+  let len = phone_number.length;
+  for (let i = 0; i < len; i++) {
+    if (i < len - 4) {
+      str += '*';
+      continue;
+    }
+    str += phone_number[i];
+  }
+  return str;
 }
